62e52ceebce561d1d6461589f47dbdf04ad886ac
Yeojin Kim <yeojin.dev@gmail.com>
2021-10-20 19:34:22 +0900

    얼굴 탐지한 이후 토픽으로 전송

--- producer/face_recognition.py
+++ producer/face_recognition.py
@@ -1,9 +1,15 @@
+import base64
+
 import cv2
 import dlib
+import numpy as np
+from confluent_kafka import Producer
 
 detector = dlib.get_frontal_face_detector()
 # cap = cv2.VideoCapture(0)  # webcam
 cap = cv2.VideoCapture('./sample.mp4')  # video
+
+p = Producer({'bootstrap.servers': 'localhost:9092'})
 
 while True:
     ret, img = cap.read()
@@ -20,11 +26,23 @@ while True:
             x2 = det.right()
             y2 = det.bottom()
 
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            # draw a face area
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
-        except Exception:
+            # TODO: 얼굴 탐지를 kafka streams 기능을 사용할 수 없는지
+            face_crop_img: np.ndarray = img[y1:y2, x1:x2]
+
+            # TODO: 이미지를 시리얼라이즈하는 방법을 찾아보기
+            face_crop_str = base64.b64encode(face_crop_img.tobytes())
+
+            # TODO: 코드 구조를 생각해보기
+            p.produce('mytopic', key='hello', value=face_crop_img.tobytes())
+            p.flush(30)  # 하나씩 전송함을 보장
+
+        except Exception as e:
+            print(e)
             pass
 
-    cv2.imshow('result', img)
+    cv2.imshow('result', face_crop_img)
     if cv2.waitKey(1) == ord('q'):
         break
