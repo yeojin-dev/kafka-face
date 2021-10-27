7f691e6b42fb594483d0485c44a093b91bf54f58
Yeojin Kim <yeojin.dev@gmail.com>
2021-10-27 19:38:45 +0900

    faust 데모 추가

---
+++ demo/streams.py
@@ -0,0 +1,15 @@
+import faust
+
+app = faust.App(
+    'hello-world',
+    broker='kafka://localhost:9092',
+    value_serializer='raw',
+)
+
+greetings_topic = app.topic('mytopic')
+
+
+@app.agent(greetings_topic)
+async def greet(greetings):
+    async for greeting in greetings:
+        print(greeting)
